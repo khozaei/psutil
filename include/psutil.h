471bed6aad72b44ea5f5c2a7d09ac7dc9015d9ba
Amin Khozaei <amin.khozaei@gmail.com>
2022-11-06 15:50:22 +0330

    psutil: license notice in files

--- include/psutil.h
+++ include/psutil.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022, Amin Khozaei <amin.khozaei@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
 #include <stdint.h>
 
 #define DEFAULT_BUFFER_LEN 1024
--- src/psutil.c
+++ src/psutil.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2022, Amin Khozaei <amin.khozaei@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
 #include "psutil.h"
 
 #include <stdlib.h>
